557ef7067168314f3cff8a4ca4c508e21aa03874
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-02-09 19:55:10 +0500

    Cart Added

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import headphone1 from './1001.png';
+import headphone2 from './1002.png';
+import headphone3 from './1003.png';
+import headphone4 from './1004.png';
+import headphone5 from './1005.png';
+import headphone6 from './1006.png';
+
+export {
+    headphone1,
+    headphone2,
+    headphone3,
+    headphone4,
+    headphone5,
+    headphone6,
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ProductCard from '../components/ProductCard'
+import { headphone1, headphone2, headphone3, headphone4, headphone5, headphone6 } from '../assets/images'
+
+const Home = () => {
+
+
+    const products = [
+        {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": headphone1},
+        {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": headphone2},
+        {"id": 3, "name": "JBL Tune 760NC", "price": 179, "image": headphone3},
+        {"id": 4, "name": "Logitech H111 Wired", "price": 39, "image": headphone4},
+        {"id": 5, "name": "APPLE Airpods Max Bluetooth Headset", "price": 199, "image": headphone5},
+        {"id": 6, "name": "ZEBRONICS Zeb-Thunder Wired", "price": 29, "image": headphone6},
+      ]
+
+
+
+
+
+  return (
+    <section className='mt-20'>
+        <div className="flex flex-wrap justify-center gap-5">
+        {
+            products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))
+        }
+        </div>
+    </section>
+  )
+}
+
+export default Home
