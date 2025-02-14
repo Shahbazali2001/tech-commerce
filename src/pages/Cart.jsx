085d47f186d29a8bd4f8c559cc47f7d05536f5fa
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-02-14 20:06:55 +0500

    Cart Updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import CartCard from '../components/CartCard'
 import useTitle from '../hooks/useTitle'
+import { useCart } from '../context/CartContext'
 
 const Cart = () => {
+
+
+  const {total, cartList} = useCart;
   useTitle('Cart')
   return (
-    <div>Cart</div>
+    <section className='cart'>
+      <h1>Cart Items: {cartList.length} / Total: ${total}</h1>
+      <div>
+        {cartList.map((product) => (
+          <CartCard key={product.id} product={product} />
+        ))}
+      </div>
+
+    </section>
   )
 }
 
