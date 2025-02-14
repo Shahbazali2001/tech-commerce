fab959b5625ef32f9e873ac34ec54f02009dc349
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-02-14 19:53:23 +0500

    Shopping Cart Update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -33,8 +33,8 @@ const ProductCard = ({ product }) => {
             <h3 className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             ${price}
             </h3>
-            <a
-            href="#"
+            <div
+          
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
              { isInCart ? (<button className="remove" onClick={() => removeFromCart(product)}>Remove</button>) :  (<button onClick={() => addToCart(product)}>Add To Cart</button>) }
@@ -53,7 +53,7 @@ const ProductCard = ({ product }) => {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
             </svg>
-            </a>
+            </div>
         </div>
       </div>
     </div>
